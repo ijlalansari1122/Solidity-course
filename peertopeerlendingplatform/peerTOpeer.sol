a648af3f3f14ff091a047cd59d5b670044f41289
ijlalansari1122 <ansariijlal90@gmail.com>
2023-08-30 02:28:11 +0500

    Peertopeerlendingplatform

--- peertopeerlendingplatform/peerTOpeer.sol
+++ peertopeerlendingplatform/peerTOpeer.sol
@@ -4,49 +4,27 @@ pragma solidity ^0.8.17;
 import "./safeMath.sol";
 import "./Credit.sol";
 
-contract  peerTOpeer is Ownable {
+contract peerTOpeer is Ownable {
+    using safeMath for uint256;
 
+    // user managment
 
-using safeMath for uint;
+    struct User {
+        bool credited;
+        address activeCredit;
+        bool Fraudstatus;
+        address[] allCredits;
+    }
 
+    // Storing data
 
-// user managment
+    // all users data in mapping
 
-struct User{
+    mapping(address => User) public users;
 
-bool credited;
-address activeCredit;
-bool Fraudstatus;
-address[] allCredits;
+    // Array of all credits addresses
 
+    address[] public credits;
 
-}
-
-
-
-// Storing data
-
-// all users data in mapping
-
-mapping (address => User) public  users;
-
-// Array of all credits addresses
-
-address[] public  credits;
-
-
-
-
-// EVENTS
-
-
-
-
-
-
-
-
-
-
-
+    // EVENTS
 }
